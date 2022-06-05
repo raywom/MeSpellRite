bca01e40b24c88c10b008ebd9253eb3cc2ef3726
@raywom <40425058+raywom@users.noreply.github.com>
2022-06-05 22:08:55 +0200

    fix

--- main.cpp
+++ main.cpp
@@ -11,15 +11,15 @@ int main() {
     string word;
     ifstream words(R"(C:\Users\rkuan\CLionProjects\MeSpellRite\words.txt)");
     using chrono::nanoseconds;
-    nanoseconds totalNaive(0), totalTree(0), totalTrie(0), totalHashtable(0), dictionaryNaive(0),dictionaryTree(0), dictionaryTrie(0), dictionaryHashtable(0);
-    ofstream results("ex1_results.txt"), dictionariesResults("ex1_results_dictionaries.txt");
+    nanoseconds totalNaive(0), totalTree(0), totalTrie(0), totalHashtable(0), dictionaryNaive(0), dictionaryTree(
+            0), dictionaryTrie(0), dictionaryHashtable(0);
+    ofstream results("ex1_results_search.txt"), dictionariesResults("ex1_results_inserts.csv");
     results << "size		Naive		BBST		Trie		HashTable" << endl;
     dictionariesResults << "size		Naive		BBST		Trie		Hashtable" << endl;
 
     int dictionarySize = 0;
 
-    while (getline(words, word))
-    {
+    while (getline(words, word)) {
         ++dictionarySize;
     }
     words.clear();
@@ -27,121 +27,114 @@ int main() {
 
     cout << dictionarySize << endl;
 
-    auto* tree = new BBSTNode("a");
+    auto *tree = new BBSTNode("a");
     Trie trie1;
     Hashtable table;
     list<string> naive;
 
     //Dictionary building
-    int j = 0;
-    for (int i = 1; i <= 4; i++)
-    {
-        for (; j < 2000 * i; )
-        {
-            getline(words, word);
 
-            //Naive
-            auto start = chrono::steady_clock::now();
-            naive.push_back(word);
-            auto end = chrono::steady_clock::now();
-            dictionaryNaive = dictionaryNaive + end - start;
+    for (int j = 0; j < dictionarySize; j++) {
+        getline(words, word);
 
-            //BBST
-            start = chrono::steady_clock::now();
-            tree = insert(tree, word);
-            //tree.insert(word);
-            end = chrono::steady_clock::now();
-            dictionaryTree = dictionaryTree + end - start;
+        //Naive
+        auto start = chrono::steady_clock::now();
+        naive.push_back(word);
+        auto end = chrono::steady_clock::now();
+        dictionaryNaive += end - start;
+
+        //BBST
+        start = chrono::steady_clock::now();
+        tree = insert(tree, word);
+        //tree.insert(word);
+        end = chrono::steady_clock::now();
+        dictionaryTree += end - start;
 
 
-            //Trie
-            start = chrono::steady_clock::now();
-            trie1.insert(word);
-            end = chrono::steady_clock::now();
-            dictionaryTrie = dictionaryTrie + end - start;
+        //Trie
+        start = chrono::steady_clock::now();
+        trie1.insert(word);
+        end = chrono::steady_clock::now();
+        dictionaryTrie += end - start;
 
-            //Hashtable
-            start = chrono::steady_clock::now();
-            table.insertString(word);
-            end = chrono::steady_clock::now();
-            dictionaryHashtable = dictionaryHashtable + end - start;
+        //Hashtable
+        start = chrono::steady_clock::now();
+        table.insertString(word);
+        end = chrono::steady_clock::now();
+        dictionaryHashtable += end - start;
 
-            j += word.length();
-        }
         dictionariesResults << j << "		" << dictionaryNaive.count() << "		" << dictionaryTree.count()
-                            << "	" << dictionaryTrie.count() << "		" << dictionaryHashtable.count() << endl;
+                            << "	" << dictionaryTrie.count() << "		" << dictionaryHashtable.count()
+                            << endl;
     }
+
+
 
     //Search
     ifstream text(R"(C:\Users\rkuan\CLionProjects\MeSpellRite\text.txt)");
     string spell, wordsFromSpell;
-    int resultNaive;
 
     int sizeSpell = 0;
-    while (getline(text, spell))
-    {
+    while (getline(text, spell)) {
         ++sizeSpell;
     }
     text.clear();
     text.seekg(0);
 
-    int sizeSpellIterate = sizeSpell;
     int sizeSpellChecked = 0;
 
-    for (int i = 1; i <= 4; i++)
-    {
-        for (j = 0; j < sizeSpellIterate * i; j++)
-        {
-            getline(text, spell);
-            sizeSpellChecked += spell.length();
-            for (char k : spell)
-            {
-                if ((k >= 65 && k <= 90) || (k >= 97 && k <= 122))
-                {
-                    wordsFromSpell += k;
-                    continue;
-                }
-
-                words.clear();
-                words.seekg(0);
-
-                //Naive algorithm
-                auto start = chrono::steady_clock::now();
-                for (auto & it : naive)
-                {
-                    if (it == wordsFromSpell)
-                        break;
-                }
-                auto end = chrono::steady_clock::now();
-                totalNaive += end - start;
-
-                //BBST
-                start = chrono::steady_clock::now();
-                search(tree, wordsFromSpell);
-                //tree.find(wordsFromSpell);
-                end = chrono::steady_clock::now();
-                totalTree += end - start;
-
-                //Trie
-                start = chrono::steady_clock::now();
-                trie1.search(wordsFromSpell);
-                end = chrono::steady_clock::now();
-                totalTrie += end - start;
-
-
-                //Hashtable
-                start = chrono::steady_clock::now();
-                table.searchTable(wordsFromSpell);
-                end = chrono::steady_clock::now();
-                totalHashtable += end - start;
-
-                wordsFromSpell = "";
+    int countOfWords=0;
+    for (int j = 0; j < sizeSpell; j++) {
+        getline(text, spell);
+        sizeSpellChecked += spell.length();
+        for (char k: spell) {
+            if ((k >= 65 && k <= 90) || (k >= 97 && k <= 122)) {
+                wordsFromSpell += k;
+                continue;
             }
-        }
+            countOfWords++;
 
-        results << sizeSpellChecked << "    " << totalNaive.count() << "		" << totalTree.count() << "		"
-                << totalTrie.count() << "		" << totalHashtable.count() << endl;
+            words.clear();
+            words.seekg(0);
+
+            //Naive algorithm
+            auto start = chrono::steady_clock::now();
+            for (auto &it: naive) {
+                if (it == wordsFromSpell)
+                    break;
+            }
+            auto end = chrono::steady_clock::now();
+            totalNaive += end - start;
+
+            //BBST
+            start = chrono::steady_clock::now();
+            search(tree, wordsFromSpell);
+            //tree.find(wordsFromSpell);
+            end = chrono::steady_clock::now();
+            totalTree += end - start;
+
+            //Trie
+            start = chrono::steady_clock::now();
+            trie1.search(wordsFromSpell);
+            end = chrono::steady_clock::now();
+            totalTrie += end - start;
+
+
+            //Hashtable
+            start = chrono::steady_clock::now();
+            table.searchTable(wordsFromSpell);
+            end = chrono::steady_clock::now();
+            totalHashtable += end - start;
+
+            wordsFromSpell = "";
+            results << countOfWords << "\t" << totalNaive.count() << "		" << totalTree.count() << "		"
+                    << totalTrie.count() << "		" << totalHashtable.count() << endl;
+        }
     }
+
+
+
+
 
     words.close();
     return 0;
